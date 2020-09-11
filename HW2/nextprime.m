04eee14b086f2241b4c6f2262e83eec7d2e5cd39
Andrew Player <andrewplayer3@gmail.com>
2020-09-11 11:58:46 -0800

    files for hw2

--- HW2/nextprime.m
+++ HW2/nextprime.m
@@ -1,4 +1,10 @@
-function y=nextprime(x)
+function y = nextprime(x)
+    %isprime does not work for negative numbers
+    %so we just return 2, the first prime number.
+    if(x < 2)
+        y = 2;
+        return
+    end
     while ~isprime(x)
         x = x + 1;
     end
