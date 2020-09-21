a18455857485034f7fa07654d2c1122bcb178b85
Andrew Player <andrewplayer3@gmail.com>
2020-09-20 16:45:42 -0800

    HW3 Files

---
+++ HW3/newton.m
@@ -0,0 +1,36 @@
+function [r,hist] = hw3newton(f, fp, x1, ftol, xtol, Nmax)
+    
+    %X_{k}
+    current_value = x1;
+    %X_{k-1}
+    last_value = x1;
+    
+    iterations = 0;
+    hist = [];
+    
+    while(true)
+        if(iterations > Nmax)
+            error("Iteration Count Exceeded")
+        end
+        
+        % Check if f(X_k) is sufficiently small
+        if(abs(f(last_value)) < ftol)
+            r = last_value;
+            return
+        end
+        
+        % Do the iteration
+        last_value = current_value;
+        current_value = current_value - f(current_value)/fp(current_value);
+        iterations = iterations + 1;
+        
+        % Check if |X_(k) - X_(k-1)| is sufficiently small
+        if(abs(current_value - last_value) < xtol)
+            r = current_value;
+            return
+        end
+        
+        hist = [hist ;last_value];
+    end
+end
+
