2525510046908a36847c11c09cabd5cdc3ec582b
Andrew Player <andrewplayer3@gmail.com>
2020-11-18 21:36:38 -0600

    HW12 Stuff

---
+++ Functions/quad4th.m
@@ -0,0 +1,13 @@
+function Q = quad4th(f, a, b, x)
+    n = length(x);
+    B = zeros(n, n);
+    bv = zeros(n, 1);
+    for i = 1:n
+        B(i,:) = x.^(i-1);
+        bv(i) = b^(i)/i - a^(i)/i;
+    end
+    A = B\bv;
+    disp(A);
+    Q = arrayfun(f,x)*A;
+end
+
